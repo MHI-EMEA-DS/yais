6bb60ca672729bb8bc3d8ec9f5ac9b503998669c
wojtek-mhi <wojciech.jasinski@ds.mhie.com>
2023-10-26 11:44:31 +0200

    use null value

--- yais.sh
+++ yais.sh
@@ -384,11 +384,7 @@ persistence_template="[
         \"Id\": \"${service_id}\",
         \"Name\": \"${ARG_MAIN_SERVICE_NAME}\",
         \"WorkingDir\": \"${ARG_MAIN_SERVICE_DIR}\",
-        \"WebAccess\": {
-          \"Hostname\": \"\",
-          \"Protocol\": \"\",
-          \"Port\": 0,
-        },
+        \"WebAccess\": null,
         \"Chart\": \"${ARG_MAIN_SERVICE_CHART}\",
         \"Values\": \"${valuesContent}\",
         \"CreatedAt\": \"${current_time}\",
