1f7b7b960216b0c033a669de7e7d2cc6c4deff15
andrei_sinianok <andrei@krafteq.de>
2023-11-20 11:41:29 +0300

    add curl output

--- chartman.sh
+++ chartman.sh
@@ -19,24 +19,24 @@ SCRIPT_LOCATION="/usr/local/bin/chartman"
 TMP_FILE=$(mktemp -p "" "XXXXX.sh")
 
 curl -s -L "$SCRIPT_URL" > "$TMP_FILE"
+curl_output=$(curl -s "$url")
 
-diff_output=$(diff -q <(echo "$TMP_FILE") "$SCRIPT_LOCATION")
+diff_output=$(diff -q <(echo "$curl_output") "$SCRIPT_LOCATION")
 
 if [ $? -eq 0 ]; then
   rm -f "$TMP_FILE"
 else
-  echo "New version available"
+  echo "New version of script available"
   ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
   read -p "Do you want to download and replace the file? (y/n) " choice
   if [[ $choice == "y" || $choice == "Y" ]]; then
     echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > updater.sh
     echo "rm -f \"$TMP_FILE\"" >> updater.sh
+    echo "echo "new version downloaded""
 
     chmod +x ~/updater.sh
     chmod +x "$TMP_FILE"
     exec ~/updater.sh
-    update "$@"
-    echo "$@"
   fi
 fi
 
