71be745530d8a3ca208c5983530d2441fe0ee235
andrei_sinianok <andrei@krafteq.de>
2023-11-20 12:38:49 +0300

    fix

--- chartman.sh
+++ chartman.sh
@@ -16,10 +16,7 @@ fi
 
 SCRIPT_URL="https://raw.githubusercontent.com/MHI-EMEA-DS/yais/GCCP-7839/chartman.sh"
 SCRIPT_LOCATION="/usr/local/bin/chartman"
-TMP_FILE=$(mktemp -p "" "XXXXX.sh")
 
-
-curl -s -L "$SCRIPT_URL" > "$TMP_FILE"
 curl_output=$(curl -s "$url")
 diff_output=$(diff -q <(echo "$curl_output") "$SCRIPT_LOCATION")
 
@@ -27,15 +24,21 @@ if [ $? -eq 0 ]; then
   echo "You use latest version of script"
 else
   echo "New version of script available"
+  TMP_FILE=$(mktemp -p "" "XXXXX.sh")
+  curl -s -L "$SCRIPT_URL" > "$TMP_FILE"
   ABS_SCRIPT_PATH=$(readlink -f "$SCRIPT_LOCATION")
   read -p "Do you want to download and replace the file? (y/n) " choice
   if [[ $choice == "y" || $choice == "Y" ]]; then
     echo "cp \"$TMP_FILE\" \"$ABS_SCRIPT_PATH\"" > updater.sh
     echo "rm -f \"$TMP_FILE\"" >> updater.sh
+    echo "echo Running script again: `basename ${SCRIPT_LOCATION}`" >> updater.sh
+    echo "exec \"$ABS_SCRIPT_PATH\"" >> updater.sh
 
     chmod +x ~/updater.sh
     chmod +x "$TMP_FILE"
     exec ~/updater.sh
+  else
+    rm -f "$TMP_FILE"
   fi
 fi
 
